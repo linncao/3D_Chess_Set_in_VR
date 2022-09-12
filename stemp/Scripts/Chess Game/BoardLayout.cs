7caa8c02a8a3f042e2a8eca15cfb9d19d8933eb9
linncao53 <lcaong24@colby.edu>
2022-09-11 21:11:01 -0400

    add Logs, Packages, ProjectSettings, Temp

---
+++ stemp/Scripts/Chess Game/BoardLayout.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Board/Layout")]
+public class BoardLayout : ScriptableObject
+{
+    [Serializable]
+    private class BoardSetup
+    {
+        public Vector2Int position;
+        public PieceType pieceType;
+        public TeamColor teamColor;
+    }
+
+    [SerializeField] private BoardSquareSetup[] boardSquares;
+
+    public int GetPiecesCount()
+    {
+        return boardSquares.Length;
+    }
+
+    public Vector2Int GetSquareCoordsAtIndex(int index)
+    {
+        if (boardSquares.Length <= index)
+        {
+            Debug.LogError("Index of piece is out of range");
+            return new Vector2Int(-1, -1);
+        }
+        return new Vector2Int(boardSquares[index].position.x - 1, boardSquares[index].position.y - 1);
+    }
+
+    public string GetSquarePieceNameAtIndex(int index)
+    {
+
+    }
+
+    public TeamColor GetSquareTeamColorAtIndex(int index)
+    {
+
+    }
+}
